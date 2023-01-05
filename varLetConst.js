b60fe368d396316cb0484db03aa7f35351dc577e
jeremy <jjliv90@gmail.com>
2023-01-04 22:19:33 -0500

    new file:   varLetConst.js

---
+++ varLetConst.js
@@ -0,0 +1,19 @@
+//instead of var PI = 3.14;
+//Pi = 42
+
+//Do This
+const PI = 3.14;
+PI = 42;
+
+
+// you can redeclare and reassign var 
+//var can be used to hoist a variable
+
+//let creates block scope
+//cannot redeclare let
+
+// cannot redeclare or reassign const
+// const creates block scope
+
+// variable are hoisted to the top of scope they are declared in with var
+// can access a hoisted variable as undefined before given its value
